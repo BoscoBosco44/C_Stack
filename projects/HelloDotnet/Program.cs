f2e530c8334705c1aac815003fdae3e71b83a4a7
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-04-02 12:33:18 -0500

    learning

--- projects/HelloDotnet/Program.cs
+++ projects/HelloDotnet/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+// string name = "boscoBosco";
+
+// char name2 = 'b';
+
+// Console.WriteLine($"name: {name}, name2: {name2}");
+
+
+for(int i = 1; i <= 255; i++) {
+    Console.WriteLine(i);
+}
+
+Random rand = new Random();
+int sum = 0;
+for(int i = 0; i < 5; i++) {
+    sum = sum + rand.Next(5, 11);
+}
+Console.WriteLine(sum);
+
+for(int i = 1; i <= 100; i++) {
+    if((i % 3 == 0 && !(i % 5 == 0) ) || (i % 5 == 0 && !(i % 3 == 0))) {
+        Console.WriteLine(i);
+    }
+}
+
+for(int i = 1; i <= 100; i++) {
+    if(i % 3 == 0 && !(i % 5 == 0)) {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0 && !(i % 3 == 0)) {
+        Console.WriteLine("buzz");
+    }
+    else if (i % 3 == 0 && i % 5 ==0) {
+        Console.WriteLine("FizzBuzz");
+    }
+}
---
+++ projects/fundaentals2/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] zeroToNine = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+string[] names = {"Tim", "Martin", "Nikki", "Sata"};
+
+bool[] arr = new bool[10];
+int i = 0;
+bool tf = true;
+while(i < 10) {
+    arr[i] = tf;
+
+    i++;
+    tf = !tf;
+}
+Console.WriteLine(arr); //wut?
+
+List<string> iceream = new List<string>();
+iceream.Add("Vanilla");
+iceream.Add("Chocolate");
+iceream.Add("Blue");
+iceream.Add("Moose Tracks");
+iceream.Add("Mint");
+
+Console.WriteLine(iceream.Count);
+Console.WriteLine(iceream[2]);
+
+iceream.RemoveAt(2);
+Console.WriteLine(iceream.Count);
+
+
+Dictionary<string, string> dict = new Dictionary<string, string>();
+
+int j = 0;
+while(j < iceream.Count) {
+    dict.Add(names[j], iceream[j]);
+    j++;
+}
+
+foreach(KeyValuePair<string, string> entry in dict) {
+    Console.WriteLine(entry);
+}
