ddfb485560128c7b48774dcf3d01f9fd777a8cc3
BoscoBosco44 <81829213+BoscoBosco44@users.noreply.github.com>
2024-04-10 13:27:21 -0500

    gitignore

---
+++ MvcProjects/FirstConnection/Models/PetContext.cs
@@ -0,0 +1,13 @@
+#pragma warning disable CS8618
+
+
+using Microsoft.EntityFrameworkCore;
+namespace FirstConnection.Models;
+
+
+public class PetContext : DbContext
+{
+    public PetContext(DbContextOptions options) : base(options) {}
+
+    public DbSet<Pet> Pets {get; set;}
+}
